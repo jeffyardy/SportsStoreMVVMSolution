1f12e23839e9c9308a010781527b55b40859e7c6
admin <admin@M-05>
2017-10-14 17:43:26 +0530

    Add project files.

---
+++ SportStoreDomainLibrary/Abstract/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using SportStoreDomainLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportStoreDomainLibrary.Abstract
+{
+  public interface IProductRepository
+  {
+    Task<List<Product>> GetProductsAsync();
+    Task<List<Product>> GetProductsByCategoryAsync(string categoryName);
+
+    Task<Product> GetProductAsync(int ProductId);
+    Task<Product> AddProductAsync(Product product);
+    Task<Product> UpdateProductAsync(Product product);
+
+    Task DeleteProductAsync(int productId);
+
+  }
+}
---
+++ SportStoreDomainLibrary/Concrete/SportsStoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportStoreDomainLibrary.Entities;
+namespace SportStoreDomainLibrary.Concrete
+{
+  public class SportsStoreDbContext : DbContext
+  {
+    public SportsStoreDbContext() : base("SportsStoreConnection") { }
+
+    public DbSet<Product> Products { get; set; }
+  }
+}
